5df2d3c32cfb7dfffe9d38c11bcebaa6debbf0c7
RAJANESH-tech <rajaneshs2006@gmail.com>
2026-07-14 12:08:06 +0530

    Time: 3 ms (56.83%), Space: 44.8 MB (89.57%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    static Set<List<Integer>> a=new HashSet<>();
+    static List<List<Integer>> a=new ArrayList<>();
     ArrayList<Integer> b=new ArrayList<>();
     static void f(int x,List<Integer> c,int []nums)
     {
@@ -11,6 +11,7 @@ class Solution {
         c.add(nums[x]);
         f(x+1,c,nums);
         c.remove(c.size()-1);
+        while(x+1<nums.length && nums[x]==nums[x+1]) x+=1;
         f(x+1,c,nums);
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
@@ -18,6 +19,6 @@ class Solution {
         b.clear();
         Arrays.sort(nums);
         f(0,b,nums);
-        return new ArrayList<>(a);
+        return a;
     }
 }
